8ad94c9429f2189b274a7923690455925525a199
Sabardi <Sabardibahari@gmail.com>
2024-05-12 09:03:06 +0800

    add file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        17: '70px'
+      },
+      fontFamily: {
+        inter: ['Alkalami'],
+      },
+      colors:{
+        primary: '#f72020'
+      }
+    },
   },
   plugins: [],
 }
